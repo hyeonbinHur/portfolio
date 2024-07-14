7a1386d3796a9b0f241c1747d5d4dfbe1b3e5a38
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-15 00:16:45 +0900

    about section done

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -3,41 +3,61 @@ export default function AboutMe() {
         <>
             <div className="row">
                 <div className="col-1-of-3">
-                    <div className="card">
-                        <div className="card--side">
-                            <div className="card--front">
-                                <div className="card--img"></div>
-                                <div className="card--name">Responsibility</div>
-                            </div>
-                            <div className="card--back">
-                                <p className="card--paragraph"></p>
-                            </div>
+                    <div className="card__1">
+                        <div className="card--front__1 card--side">
+                            <div className="card--img__1"></div>
+                            <div className="card--name">Responsibility</div>
+                        </div>
+                        <div className="card--back__1 card--side">
+                            <p className="card--paragraph">
+                                Every success and improvement come from personal
+                                responsibility. When I consistently commit to
+                                and fulfill my assigned tasks, I can finally
+                                achieve progress. To keep moving forward, I
+                                complete my daily goals, and at the end of the
+                                day, this achievement brings me happiness.
+                            </p>
                         </div>
                     </div>
                 </div>
+
                 <div className="col-1-of-3">
-                    <div className="card">
-                        <div className="card--side">
-                            <div className="card--front">
-                                <div className="card--img"></div>
-                                <div className="card--name">Responsibility</div>
-                            </div>
-                            <div className="card--back">
-                                <p className="card--paragraph"></p>
-                            </div>
+                    <div className="card__2">
+                        <div className="card--front__2 card--side">
+                            <div className="card--img__2"></div>
+                            <div className="card--name">Optimistic</div>
+                        </div>
+                        <div className="card--back__2 card--side">
+                            <p className="card--paragraph">
+                                I have always interacted with people with a
+                                positive mindset and a cheerful demeanor.
+                                Positive energy makes those around me happy, and
+                                seeing their joy brings me additional pleasure.
+                                I enjoy coding and creating programs. I study to
+                                meet new people and want to discover new joys
+                                from new faces in new places.
+                            </p>
                         </div>
                     </div>
                 </div>
+
                 <div className="col-1-of-3">
-                    <div className="card">
-                        <div className="card--side">
-                            <div className="card--front">
-                                <div className="card--img"></div>
-                                <div className="card--name">Responsibility</div>
-                            </div>
-                            <div className="card--back">
-                                <p className="card--paragraph"></p>
-                            </div>
+                    <div className="card__3">
+                        <div className="card--front__3 card--side">
+                            <div className="card--img__3"></div>
+                            <div className="card--name">Adventuros</div>
+                        </div>
+                        <div className="card--back__3 card--side">
+                            <p className="card--paragraph">
+                                Although studying and coding at my desk is
+                                certainly enjoyable, I sometimes like to travel
+                                and visit new places. I explore new locations,
+                                try out new restaurants, delve into new
+                                technologies, and acquire new knowledge. A
+                                spirit of adventure has always propelled my
+                                development, and I am currently enjoying another
+                                challenge by starting my career.
+                            </p>
                         </div>
                     </div>
                 </div>
