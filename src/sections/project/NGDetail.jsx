e17ec7422558a1e4b8620c40d64044e1869f868f
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-08-12 21:22:54 +0900

    new deploy version check

--- src/sections/project/NGDetail.jsx
+++ src/sections/project/NGDetail.jsx
@@ -22,7 +22,6 @@ import { openVideoModal } from '../../store/videoSlice';
 
 export default function NGDetail() {
     const dispatch = useDispatch();
-
     // const handleOpenImageModal = (topic) => {
     //     dispatch(openImageModal({ topic: topic }));
     // };
@@ -33,131 +32,174 @@ export default function NGDetail() {
 
     return (
         <div className="detail--content__4">
-            <label htmlFor="detail_toggle__4">
-                <span className="detail--close">
-                    <AiOutlineClose />
-                </span>
-            </label>
+            <div>
+                <label htmlFor="detail_toggle__4">
+                    <span className="detail--close">
+                        <AiOutlineClose />
+                    </span>
+                </label>
 
-            <header className="content__header">
-                <img src={NG} alt="" className="content__logo__ng" />
-            </header>
+                <header className="content__header">
+                    <img src={NG} alt="" className="content__logo__ng" />
+                </header>
 
-            <main className="content--body">
-                <div className="content--body__1">
-                    <h3 className="heading--tertiary">Features</h3>
-                    <ul className="content__ul">
-                        <li className="content--features__li">
-                            Authentication & Authorization
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() => handleOpenVideoModal('NG_Auth')}
-                            />
-                        </li>
-                        <li className="content--features__li">
-                            Sidebar Feature
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() => handleOpenVideoModal('NG_Side')}
-                            />
-                        </li>
-                        <li className="content--features__li">
-                            AI chatting
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() => handleOpenVideoModal('NG_AI')}
-                            />
-                        </li>
-                        <li className="content--features__li">
-                            DetailPage
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() =>
-                                    handleOpenVideoModal('NG_Detail')
-                                }
-                            />
-                        </li>
-                        <li className="content--features__li">
-                            Error Handling
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() => handleOpenVideoModal('NG_ERROR')}
-                            />
-                        </li>
-                        <li className="content--features__li">
-                            Responsible Design
-                            <AiFillPlaySquare
-                                className="detail__video"
-                                onClick={() =>
-                                    handleOpenVideoModal('NG_Response')
-                                }
-                            />
-                        </li>
-                    </ul>
-                </div>
-                <div className="content--body__2">
-                    <h3 className="heading--tertiary">Tools</h3>
-                    <ul className="content__ul">
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={React} />
-                            React
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={Redux} />
-                            Redux
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={Query} />
-                            React Query
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={React} />
-                            Context API
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={JS} />
-                            JavaScript
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={SCSS} />
-                            SCSS
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={AI} />
-                            Assistant API
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={RDS} />
-                            AWS-RDS
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={NAT} />
-                            AWS-NAT gateway
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={API} />
-                            AWS-API gateway
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={Lambda} />
-                            AWS-API Lambda function
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={PG} />
-                            PostgreSQL
-                        </li>
+                <main className="content--body">
+                    <div className="content--body__1">
+                        <h3 className="heading--tertiary">Features</h3>
+                        <ul className="content__ul">
+                            <li className="content--features__li">
+                                Authentication & Authorization
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_Auth')
+                                    }
+                                />
+                            </li>
+                            <li className="content--features__li">
+                                Sidebar Feature
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_Side')
+                                    }
+                                />
+                            </li>
+                            <li className="content--features__li">
+                                AI chatting
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_AI')
+                                    }
+                                />
+                            </li>
+                            <li className="content--features__li">
+                                DetailPage
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_Detail')
+                                    }
+                                />
+                            </li>
+                            <li className="content--features__li">
+                                Error Handling
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_ERROR')
+                                    }
+                                />
+                            </li>
+                            <li className="content--features__li">
+                                Responsible Design
+                                <AiFillPlaySquare
+                                    className="detail__video"
+                                    onClick={() =>
+                                        handleOpenVideoModal('NG_Response')
+                                    }
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="content--body__2">
+                        <h3 className="heading--tertiary">Tools</h3>
+                        <ul className="content__ul">
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={React}
+                                />
+                                React
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={Redux}
+                                />
+                                Redux
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={Query}
+                                />
+                                React Query
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={React}
+                                />
+                                Context API
+                            </li>
+                            <li className="content--tool__li">
+                                <img className="content--tool__logo" src={JS} />
+                                JavaScript
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={SCSS}
+                                />
+                                SCSS
+                            </li>
+                            <li className="content--tool__li">
+                                <img className="content--tool__logo" src={AI} />
+                                Assistant API
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={RDS}
+                                />
+                                AWS-RDS
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={NAT}
+                                />
+                                AWS-NAT gateway
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={API}
+                                />
+                                AWS-API gateway
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={Lambda}
+                                />
+                                AWS-API Lambda function
+                            </li>
+                            <li className="content--tool__li">
+                                <img className="content--tool__logo" src={PG} />
+                                PostgreSQL
+                            </li>
 
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={JWT} />
-                            JWT
-                        </li>
-                        <li className="content--tool__li">
-                            <img className="content--tool__logo" src={Node} />
-                            Node.js
-                        </li>
-                    </ul>
-                </div>
-            </main>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={JWT}
+                                />
+                                JWT
+                            </li>
+                            <li className="content--tool__li">
+                                <img
+                                    className="content--tool__logo"
+                                    src={Node}
+                                />
+                                Node.js
+                            </li>
+                        </ul>
+                    </div>
+                </main>
+            </div>
         </div>
     );
 }
