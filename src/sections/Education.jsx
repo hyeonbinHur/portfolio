7be292f323f15dac1db7da91a2a8ad5683c2d352
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-08-21 18:40:54 +0900

    desktop mode done

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -1,13 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import Android from '../assets/logos/androidstudio.png';
-import AWS from '../assets/logos/aws.png';
-import Java from '../assets/logos/java.png';
-import JS from '../assets/logos/js.png';
-import React from '../assets/logos/react.png';
-import RMIT from '../assets/logos/rmit.png';
-import SASS from '../assets/logos/sass.png';
-import Spring from '../assets/logos/spring.png';
-// import Swift from '../assets/logos/swift.png';
+import Katalon from '../assets/logos/katalon.png';
+import SolidWorks from '../assets/logos/solidworks.png';
+import ML from '../assets/logos/ml.png';
 export default function Education() {
     return (
         <>
@@ -15,73 +9,220 @@ export default function Education() {
             <div className="education--container">
                 <div className="education--text">
                     <p className="education--text__content">
-                        Java, python, c, c++, swift, android studio, spring
-                        Boot, strap, MySQL,MongoDB, Rasberry pi, Arduino uno,
-                        Jupyter notebook, Machine learning, data science,
-                        Matlab, bitbucket, katalon studio, katalon testOps,
-                        Jenkins,
+                        <ul className="experience--list">
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=nodejs&theme=light"
+                                    alt="Node.js logo"
+                                />
+                                Node.js
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=java&theme=light"
+                                    alt="Java logo"
+                                />
+                                Java
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=py&theme=light"
+                                    alt="Python logo"
+                                />
+                                Python
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=swift&theme=light"
+                                    alt="Swift logo"
+                                />
+                                Swift
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=c&theme=light"
+                                    alt="C logo"
+                                />
+                                C
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=cpp&theme=light"
+                                    alt="C++ logo"
+                                />
+                                C++
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=tailwind&theme=light"
+                                    alt="Tailwind CSS logo"
+                                />
+                                Tailwind CSS
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=mysql&theme=light"
+                                    alt="MySQL logo"
+                                />
+                                MySQL
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=mongo&theme=light"
+                                    alt="MongoDB logo"
+                                />
+                                MongoDB
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=androidstudio&theme=light"
+                                    alt="Android Studio logo"
+                                />
+                                Android stduio
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=spring&theme=light"
+                                    alt="Spring logo"
+                                />
+                                Sping boot
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=anaconda&theme=light"
+                                    alt="Anaconda logo"
+                                />
+                                Anaconda
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=raspberrypi&theme=light"
+                                    alt="Raspberry pi logo"
+                                />
+                                Raspberry pi
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=arduino&theme=light"
+                                    alt="Arduino Uno logo"
+                                />
+                                Arduino Uno
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    src={Katalon}
+                                    alt="Katalon logo"
+                                    className="experience--list__logo"
+                                />
+                                Katalon
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=jenkins&theme=light"
+                                    alt="Jenkins logo"
+                                />
+                                Jenkins
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    src={ML}
+                                    alt="Machine Learning logo"
+                                    className="experience--list__logo"
+                                />
+                                Machine Learning
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=bitbucket&theme=light"
+                                    alt="Bitbucket logo"
+                                />
+                                Bitbucket
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    src={SolidWorks}
+                                    alt="Solidworks logo"
+                                    className="experience--list__logo"
+                                />
+                                SolidWorks
+                            </li>
+                            <li className="experience--list__item">
+                                <img
+                                    className="experience--list__logo"
+                                    src="https://skillicons.dev/icons?i=figma&theme=light"
+                                    alt="Figma logo"
+                                />
+                                Figma
+                            </li>
+                        </ul>
                     </p>
                 </div>
                 <div className="education--animation">
                     <div className="row--md">
-                        <div className="col-1-of-4 education--image__container__android">
-                            <img
-                                src={Android}
-                                alt=""
-                                className="education--image__content"
-                            />
-                        </div>
-                        <div className="col-1-of-4 education--image__container__aws">
-                            <img
-                                src={AWS}
-                                alt=""
-                                className="education--image__content"
-                            />
-                        </div>
+                        <img
+                            className="col-1-of-4 education--image__container__android"
+                            src="https://skillicons.dev/icons?i=java&theme=light"
+                            alt="Java logo"
+                        />
+                        <img
+                            className="col-1-of-4 education--image__container__aws"
+                            src="https://skillicons.dev/icons?i=jenkins&theme=light"
+                            alt="Jenkins logo"
+                        />
                         <div className="col-1-of-4 education--image__container__java">
                             <img
-                                src={Java}
-                                alt=""
-                                className="education--image__content__java"
+                                className="education--swift"
+                                src="https://skillicons.dev/icons?i=swift&theme=light"
+                                alt="Swift logo"
                             />
                         </div>
-                        <div className="col-1-of-4 education--image__container__js">
-                            <img
-                                src={JS}
-                                alt=""
-                                className="education--image__content"
-                            />
-                        </div>
+                        <img
+                            className="col-1-of-4 education--image__container__js"
+                            src="https://skillicons.dev/icons?i=raspberrypi&theme=light"
+                            alt="Raspberry pi logo"
+                        />
                     </div>
                     <div className="row--md">
                         <div className="col-1-of-4 education--image__container__react">
                             <img
-                                src={React}
-                                alt=""
-                                className="education--image__content"
+                                className="education--arduino"
+                                src="https://skillicons.dev/icons?i=arduino&theme=light"
+                                alt="Arduino Uno logo"
                             />
                         </div>
                         <div className="col-1-of-4 education--image__container__rmit">
                             <img
-                                src={RMIT}
-                                alt=""
-                                className="education--image__content"
+                                className="education--cpp"
+                                src="https://skillicons.dev/icons?i=cpp&theme=light"
+                                alt="C++ logo"
                             />
                         </div>
-                        <div className="col-1-of-4 education--image__container__sass">
-                            <img
-                                src={SASS}
-                                alt=""
-                                className="education--image__content"
-                            />
-                        </div>
-                        <div className="col-1-of-4 education--image__container__spring ">
-                            <img
-                                src={Spring}
-                                alt=""
-                                className="education--image__content"
-                            />
-                        </div>
+                        <img
+                            className="col-1-of-4 education--image__container__sass"
+                            src="https://skillicons.dev/icons?i=spring&theme=light"
+                            alt="Spring logo"
+                        />
+                        <img
+                            className="col-1-of-4 education--image__container__spring"
+                            src="https://skillicons.dev/icons?i=py&theme=light"
+                            alt="Python logo"
+                        />
                     </div>
                 </div>
             </div>
